564d99cebf8daad7879c53be0d7d58563c917359
mimilus <mimilusubuntu@gmail.com>
2014-04-06 00:15:51 +0200

    secret wall added

--- covers.lua
+++ covers.lua
@@ -1,6 +1,95 @@
 --castle Mod By Daniel modified by mimilus
-
 --The castle
+
+--secret_stonewall
+minetest.register_craft({
+	output = "castle:top_secret_stonewall 1",
+	recipe = {
+		{"", "", ""},
+		{"castle:cover_stonewall", "", "castle:cover_stonewall"},
+		{"", "", ""},
+	},
+})
+
+minetest.register_node("castle:secret_stonewall",{
+	description = "Stonewall secret Wall",
+	tiles = { 'castle_stonewall.png' },
+	drawtype= "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	node_box = {
+		type = "fixed",
+		fixed = {{0.4375,-0.5,-0.5,0.5,0.5,0.5},{-0.5,-0.5,-0.5,-0.4375,0.5,0.5}},
+		selection_box={
+			type="fixed",{0.4375,-0.5,-0.5,0.5,0.5,0.5},{-0.5,-0.5,-0.5,-0.4375,0.5,0.5}},
+	},
+	groups = {choppy=2,dig_immediate=2},
+})
+
+--top_secret_stonewall
+minetest.register_craft({
+	output = "castle:top_secret_stonewall 1",
+	recipe = {
+		{"", "castle:cover_stonewall", ""},
+		{"", "castle:secret_stonewall", ""},
+		{"", "", ""},
+	},
+})
+
+minetest.register_craft({
+	output = "castle:top_secret_stonewall 1",
+	recipe = {
+		{"", "castle:cover_stonewall", ""},
+		{"castle:cover_stonewall", "", "castle:cover_stonewall"},
+		{"", "", ""},
+	},
+})
+
+minetest.register_node("castle:top_secret_stonewall",{
+	description = "Stonewall Top secret Wall",
+	tiles = { 'castle_stonewall.png' },
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	node_box = {
+		type = "fixed",
+		fixed = {{0.4375,-0.5,-0.5,0.5,0.5,0.5},{-0.5,-0.5,-0.5,-0.4375,0.5,0.5},{-0.4375,0.4375,-0.5,0.4375,0.5,0.5},},
+		selection_box={
+			type="fixed",{-0.5,-0.5,-0.5,0.5,0.5,0.5},{-0.5,-0.5,-0.5,-0.4375,0.5,0.5},{-0.4375,0.4375,-0.5,0.4375,0.5,0.5},},
+	},
+	groups = {choppy=2,dig_immediate=2},
+})
+
+
+
+--castlewall
+minetest.register_craft({
+	output = "castle:cover_stonewall 16",
+	recipe = {
+		{"castle:saw", "castle:stonewall", ""},
+		{"", "", ""},
+		{"", "", ""},
+	},
+})
+
+minetest.register_node("castle:cover_stonewall", {
+	description = "Stonewall Cover",
+	tiles = { 'castle_stonewall.png' },
+    drawtype = "nodebox",
+	paramtype = "light",
+    paramtype2 = "facedir",
+    node_box={
+        type="fixed",
+        fixed = { -0.5, -0.5, 0.4375, 0.5, 0.5, 0.5 }
+    },
+    selection_box={
+        type="fixed",
+        fixed = { -0.5, -0.5, 0.4375, 0.5, 0.5, 0.5 }
+    },
+	groups = {choppy=2,dig_immediate=2},
+})
+
+
 
 --Dirt
 minetest.register_craft({
