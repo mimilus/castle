4e4ab24c48e68a0cd476fd5e90ad1d3746c180bb
Xanthin <Xanthin@users.noreply.github.com>
2014-04-07 23:04:09 +0200

    support for intllib translation mod

--- murder_hole.lua
+++ murder_hole.lua
@@ -1,14 +1,22 @@
+-- Boilerplate to support localized strings if intllib mod is installed.
+local S
+if intllib then
+    S = intllib.Getter()
+else
+    S = function(s) return s end
+end
+
 local hole = {}
 
 hole.types = {
-	{"stonewall", "Stonewall", "castle_stonewall", "castle:stonewall"},
-    {"cobble", "Cobble", "default_cobble", "default:cobble"},
-    {"stonebrick", "Stonebrick", "default_stone_brick", "default:stonebrick"},
-    {"sandstonebrick", "Sandstone Brick", "default_sandstone_brick", "default:sandstone_brick"},
-    {"desertstonebrick", "Desert Stone Brick", "default_desert_stone_brick", "default:desert_stonebrick"},
-    {"stone", "Stone", "default_stone", "default:stone"},
-    {"sandstone", "Sandstone", "default_sandstone", "default:sandstone"},
-    {"desertstone", "Desert Stone", "default_desert_stone", "default:desert_stone"},
+	{"stonewall", S("Stonewall"), "castle_stonewall", "castle:stonewall"},
+    {"cobble", S("Cobble"), "default_cobble", "default:cobble"},
+    {"stonebrick", S("Stonebrick"), "default_stone_brick", "default:stonebrick"},
+    {"sandstonebrick", S("Sandstone Brick"), "default_sandstone_brick", "default:sandstonebrick"},
+    {"desertstonebrick", S("Desert Stone Brick"), "default_desert_stone_brick", "default:desert_stonebrick"},
+    {"stone", S("Stone"), "default_stone", "default:stone"},
+    {"sandstone", S("Sandstone"), "default_sandstone", "default:sandstone"},
+    {"desertstone", S("Desert Stone"), "default_desert_stone", "default:desert_stone"},
 }
 
 for _, row in ipairs(hole.types) do
@@ -19,7 +27,7 @@ for _, row in ipairs(hole.types) do
 	-- Node Definition
 	minetest.register_node("castle:hole_"..name, {
 	    drawtype = "nodebox",
-		description = desc.." Murder Hole",
+		description = S("%s Murder Hole"):format(desc),
 		tiles = {tile..".png"},
 		groups = {cracky=3},
 		sounds = default.node_sound_stone_defaults(),
