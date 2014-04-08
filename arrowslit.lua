f772ee23ed9d3890b962b60c158c5cd94aef89fc
Xanthin <Xanthin@users.noreply.github.com>
2014-04-08 04:08:36 +0200

    changed translation

--- arrowslit.lua
+++ arrowslit.lua
@@ -13,14 +13,14 @@ minetest.register_alias("castle:arrowslit", "castle:arrowslit_stonewall_cross")
 local arrowslit = {}
 
 arrowslit.types = {
-	{"stonewall", S("Stonewall"), "castle_stonewall", "castle:stonewall"},
-    {"cobble", S("Cobble"), "default_cobble", "default:cobble"},
-    {"stonebrick", S("Stonebrick"), "default_stone_brick", "default:stonebrick"},
-    {"sandstonebrick", S("Sandstone Brick"), "default_sandstone_brick", "default:sandstonebrick"},
-    {"desertstonebrick", S("Desert Stone Brick"), "default_desert_stone_brick", "default:desert_stonebrick"},
-    {"stone", S("Stone"), "default_stone", "default:stone"},
-    {"sandstone", S("Sandstone"), "default_sandstone", "default:sandstone"},
-    {"desertstone", S("Desert Stone"), "default_desert_stone", "default:desert_stone"},
+	{"stonewall", "Stonewall", "castle_stonewall", "castle:stonewall"},
+    {"cobble", "Cobble", "default_cobble", "default:cobble"},
+    {"stonebrick", "Stonebrick", "default_stone_brick", "default:stonebrick"},
+    {"sandstonebrick", "Sandstone Brick", "default_sandstone_brick", "default:sandstone_brick"},
+    {"desertstonebrick", "Desert Stone Brick", "default_desert_stone_brick", "default:desert_stonebrick"},
+    {"stone", "Stone", "default_stone", "default:stone"},
+    {"sandstone", "Sandstone", "default_sandstone", "default:sandstone"},
+    {"desertstone", "Desert Stone", "default_desert_stone", "default:desert_stone"},
 }
 
 for _, row in ipairs(arrowslit.types) do
@@ -31,7 +31,7 @@ for _, row in ipairs(arrowslit.types) do
 	-- Node Definition
 	minetest.register_node("castle:arrowslit_"..name, {
 	    drawtype = "nodebox",
-		description = S("%s Arrowslit"):format(desc),
+		description = S("%s Arrowslit"):format(S(desc)),
 		tiles = {tile..".png"},
 		groups = {cracky=3},
 		sounds = default.node_sound_defaults(),
@@ -55,7 +55,7 @@ for _, row in ipairs(arrowslit.types) do
 	})
 	minetest.register_node("castle:arrowslit_"..name.."_cross", {
 	    drawtype = "nodebox",
-		description = S("%s Arrowslit with Cross"):format(desc),
+		description = S("%s Arrowslit with Cross"):format(S(desc)),
 		tiles = {tile..".png"},
 		groups = {cracky=3},
 		sounds = default.node_sound_defaults(),
@@ -83,7 +83,7 @@ for _, row in ipairs(arrowslit.types) do
 	})
 	minetest.register_node("castle:arrowslit_"..name.."_hole", {
 	    drawtype = "nodebox",
-		description = S("%s Arrowslit with Hole"):format(desc),
+		description = S("%s Arrowslit with Hole"):format(S(desc)),
 		tiles = {tile..".png"},
 		groups = {cracky=3},
 		sounds = default.node_sound_defaults(),
