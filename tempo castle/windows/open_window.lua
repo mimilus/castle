17f22b668d9427f067cf687f8d8d49b5c39a60bf
mimilus <mimilusubuntu@gmail.com>
2014-04-09 19:34:29 +0200

    castle cornwall

---
+++ tempo castle/windows/open_window.lua
@@ -0,0 +1,16 @@
+-- GENERATED CODE
+-- Node Box Editor, version 0.6.2 - Stone
+-- Namespace: test
+
+minetest.register_node("test:node_1",{
+	drawtype="nodebox",
+	paramtype = "light",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{0.4375,-0.5,0,0.5,0.5,0.5}, --droit
+			{-0.5,-0.5,0,-0.4375,0.5,0.5}, --gauche
+		}
+	}
+})
+
---
+++ tempo castle/windows/window_closed.lua
@@ -0,0 +1,16 @@
+-- GENERATED CODE
+-- Node Box Editor, version 0.6.2 - Stone
+-- Namespace: test
+
+minetest.register_node("test:node_1",{
+	drawtype="nodebox",
+	paramtype = "light",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{0,-0.5,0.426228,0.5,0.5,0.488728}, --droit
+			{-0.5,-0.5,0.43396,0,0.5,0.49646}, --gauche
+		}
+	}
+})
+
