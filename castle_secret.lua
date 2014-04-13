922673b9921f56dd9eba0983db88e0b517a05d6e
mimilus <mimilusubuntu@gmail.com>
2014-04-13 18:35:34 +0200

    Added some secret wall parts

--- castle_secret.lua
+++ castle_secret.lua
@@ -98,7 +98,7 @@ minetest.register_craft({
 	end
 end
 
---Top Secret
+--Top Secret Wall
 local top_secret_ = {}
 
 top_secret_.types = {
@@ -139,6 +139,104 @@ minetest.register_craft({
 		--Choose craft material
 		minetest.register_craft({
 			output = "castle:castle:top_secret_" ..name.. " 4",
+			recipe = {
+			{"",craft_material, "" },
+			{craft_material,"", craft_material},
+			{"",craft_material, ""} },
+		})
+	end
+end
+
+--Top Secret End Wall
+local top_secret_end = {}
+
+top_secret_end.types = {
+	{"stonewall", "Stonewall", "castle_stonewall", "castle:stonewall"},
+	{"cornerwall", "CornerWall", "castle_cornwall", "castle:cornerwall"},
+	{"dungeon","Dungeon Stone","castle_dungeon_stone","castle:dungeon_stone"},
+}
+
+for _, row in ipairs(top_secret_end.types) do
+	local name = row[1]
+	local desc = row[2]
+	local tile = row[3]
+	local craft_material = row[4]
+	-- Node Definition
+	minetest.register_node("castle:top_secret_end" ..name, {
+	    drawtype = "nodebox",
+		description = desc.." Top Secret End Wall",
+		tiles = {tile..".png"},
+		groups = {cracky=3},
+		sounds = default.node_sound_stone_defaults(),
+	    paramtype = "light",
+	    paramtype2 = "facedir",
+	node_box = {
+		type = "fixed",
+		fixed = {{-0.4375,-0.5,0.4375,0.4375,0.5,0.5},{0.4375,-0.5,-0.5,0.5,0.5,0.5},{-0.5,-0.5,-0.5,-0.4375,0.5,0.5},{-0.4375,0.4375,-0.5,0.4375,0.5,0.5},},
+		selection_box={{-0.4375,-0.5,0.4375,0.4375,0.5,0.5},{-0.5,-0.5,-0.5,0.5,0.5,0.5},{-0.5,-0.5,-0.5,-0.4375,0.5,0.5},{-0.4375,0.4375,-0.5,0.4375,0.5,0.5},},
+		},
+	})
+minetest.register_craft({
+	output = "castle:top_secret_end" ..name.. " 1",
+	recipe = {
+		{"", "castle:cover_" ..name,"" },
+		{"castle:cover_" ..name , "castle:cover_" ..name, "castle:cover_" ..name },
+		{"", "", ""},
+	},
+})
+	if craft_material then
+		--Choose craft material
+		minetest.register_craft({
+			output = "castle:castle:top_secret_end" ..name.. " 4",
+			recipe = {
+			{"",craft_material, "" },
+			{craft_material,"", craft_material},
+			{"",craft_material, ""} },
+		})
+	end
+end
+
+--Bottom Secret End Wall
+local bottom_secret_end = {}
+
+bottom_secret_end.types = {
+	{"stonewall", "Stonewall", "castle_stonewall", "castle:stonewall"},
+	{"cornerwall", "CornerWall", "castle_cornwall", "castle:cornerwall"},
+	{"dungeon","Dungeon Stone","castle_dungeon_stone","castle:dungeon_stone"},
+}
+
+for _, row in ipairs(bottom_secret_end.types) do
+	local name = row[1]
+	local desc = row[2]
+	local tile = row[3]
+	local craft_material = row[4]
+	-- Node Definition
+	minetest.register_node("castle:bottom_secret_end" ..name, {
+	    drawtype = "nodebox",
+		description = desc.." Bottom Secret End Wall",
+		tiles = {tile..".png"},
+		groups = {cracky=3},
+		sounds = default.node_sound_stone_defaults(),
+	    paramtype = "light",
+	    paramtype2 = "facedir",
+	node_box = {
+		type = "fixed",
+		fixed = {{-0.4375,-0.5,0.4375,0.4375,0.5,0.5},{0.4375,-0.5,-0.5,0.5,0.5,0.5},{-0.5,-0.5,-0.5,-0.4375,0.5,0.5},},
+		selection_box={{-0.4375,-0.5,0.4375,0.4375,0.5,0.5},{-0.5,-0.5,-0.5,0.5,0.5,0.5},{-0.5,-0.5,-0.5,-0.4375,0.5,0.5},},
+		},
+	})
+minetest.register_craft({
+	output = "castle:bottom_secret_end" ..name.. " 1",
+	recipe = {
+		{"", "castle:cover_" ..name,"" },
+		{"castle:cover_" ..name , "castle:cover_" ..name, "castle:cover_" ..name },
+		{"", "", ""},
+	},
+})
+	if craft_material then
+		--Choose craft material
+		minetest.register_craft({
+			output = "castle:castle:bottom_secret_end" ..name.. " 4",
 			recipe = {
 			{"",craft_material, "" },
 			{craft_material,"", craft_material},
