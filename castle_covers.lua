fa3262d60a212fd2b2a2c1b4e2c739be7e56e8b7
mimilus <mimilusubuntu@gmail.com>
2014-04-20 18:10:27 +0200

    Castle cover again

--- castle_covers.lua
+++ castle_covers.lua
@@ -1,11 +1,5 @@
 local castle_cover = {}
 
---tempo
---
---	{"cornerwall","CornerWall","castle_secret_cornwall_door_inv.png","castle:cornerwall"},
---	{"book","Bookshelf","default_bookshelf.png","castle:bookshelf"},
-
-
 castle_cover.types = {
 	{"stonewall","StoneWall","castle_stonewall.png","castle:stonewall"},
 	{"dungeon","DungeonWall","castle_dungeon_stone.png","castle:dungeonwall"},
@@ -13,6 +7,22 @@ castle_cover.types = {
 	{"bookshelf","Bookshelf","default_bookshelf.png","default:bookshelf"},
 	{"dirt","Dirt","default_dirt.png","default:dirt"},
 	{"stone","Stone","default_stone.png","default:stone"},
+	{"cobble","Cobble","default_cobble.png","default:cobble"},
+	{"sand","Sand","default_sand.png","default:sand"},
+	{"desert_sand","Desert Sand","default_desert_sand.png","default:desert_sand"},
+	{"desert_stone","Desert Stone","default_desert_stone.png","default:desert-stone"},
+	{"brick","Brick","default_brick.png","default:brick"},
+	{"steel","Steel","default_steel_block.png","default:steelblock"},
+	{"glass","Glass","default_glass.png","default:glass"},
+	{"stonebrick","Stone Brick","default_stone_brick.png","default:stonebrick"},
+	{"desert_stone_brick","Desert Stone Brick","default_desert_stone_brick.png","default:desertstonebrick"},
+	{"sand_stone_brick","Sand Stone Brick","default_sandstone_brick.png","default:sandstonebrick"},
+	{"tree","Tree","default_tree.png","default:tree"},
+	{"wood","Wood","default_wood.png","default:wood"},
+	{"sand_stone","Sand Stone","default_sandstone.png","default:sandstone"},
+	{"obsidian","Obsidian","default_obsidian.png","default:obsidian"},
+	{"gravel","Gravel","default_gravel.png","default:gravel"},
+	
 }
 
 for _, row in ipairs(castle_cover.types) do
